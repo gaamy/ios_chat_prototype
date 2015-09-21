66e8b27a6ed17923be07a0aef9513363fd2ff601
gaamy <gabriel.amyot@gmail.com>
2015-09-20 22:37:32 -0400

    Version 1.0 du prototype chat client léger fonctionnel

--- Prototype_Chat/ViewController.swift
+++ Prototype_Chat/ViewController.swift
@@ -9,71 +9,194 @@
 import UIKit
 
 //this view controller represents the chat main view controller
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSStreamDelegate  {
     
+    //UI elements
     @IBOutlet weak var lblMessage: UILabel!
-    
     @IBOutlet weak var monTexte: UITextField!
     @IBOutlet weak var messages: UITextView!
     
+    
+    //imported elements
+    var host = ""
+    var port = 0
+    var userName = ""
+    
+
+
+    /*
+        this is the first function to be called when the view loads
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //connectToTCPServer(self.host, port:self.port,userName: self.userName)
+        
+        initNetworkCommunication(self.host as CFString, connectionPort:UInt32(self.port))
+        
+        joinChat(self.userName)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    //-------Networking------//
+
+    //Client Socket
+    var inputStream: NSInputStream!
+    var outputStream: NSOutputStream!
     
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    func initNetworkCommunication(connectionHost: CFString, connectionPort: UInt32) {
+        var readstream : Unmanaged<CFReadStream>?
+        var writestream : Unmanaged<CFWriteStream>?
+        
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, connectionHost, connectionPort, &readstream, &writestream)
+        inputStream = readstream!.takeRetainedValue()
+        outputStream = writestream!.takeRetainedValue()
+        
+        inputStream.delegate = self
+        outputStream.delegate = self
+        
+        inputStream.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+        outputStream.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+        
+        inputStream.open()
+        outputStream.open()
     }
+    
+    /**
+    @input: userName
+    @output:
     */
-
-
+    func joinChat(user: String) {
+        var response: String = "iam:\(user)"
+        let data: NSData = response.dataUsingEncoding(NSASCIIStringEncoding)!
+        outputStream.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length)
+    }
+    
+    func quitChat(){
+        
+        inputStream.close()
+        outputStream.close()
+    }
+    
+    
     @IBAction func envoyer(sender: AnyObject) {
-       
+        //send the message to the server
+        
+        //TODO: verifier que la connection a ete etablie avant d'envoyer le message
+        var response: String = "msg:\(monTexte.text)"
+        monTexte.text = ""
+        
+        let data: NSData = response.dataUsingEncoding(NSASCIIStringEncoding)!
+        
+        self.outputStream.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length)
+        
+        /*
+        if success{
+            println("message sent successfully")
+        }
+        else {
+            println("message not sent (connexion error)")
+            println(errorMsg)
+        }
+        */
+        
+        monTexte.text = "";
+    }
+    
+  
+    
+    /**stream()
+    handle the NSStream events.
+    It's here where the incomming TCP messages are handled
+    */
+    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
+        switch (eventCode){
+        case NSStreamEvent.ErrorOccurred:
+            NSLog("ErrorOccurred")
+            break
+        case NSStreamEvent.EndEncountered:
+            NSLog("EndEncountered")
+            break
+        case NSStreamEvent.None:
+            NSLog("None")
+            break
+        case NSStreamEvent.HasBytesAvailable:
+            NSLog("HasBytesAvaible")
+            var buffer = [UInt8](count: 4096, repeatedValue: 0)
+            if ( aStream == inputStream){
+                
+                while (inputStream.hasBytesAvailable){
+                    var len = inputStream.read(&buffer, maxLength: buffer.count)
+                    if(len > 0){
+                        var output = NSString(bytes: &buffer, length: buffer.count, encoding: NSUTF8StringEncoding)
+                        if (output != ""){
+                            println(output!)
+                            var stringOutput = output as! String
+                            updateChatView(stringOutput)
+                            NSLog("server said: %@", output!)
+                            
+                        }
+                    }
+                }
+            }
+            break
+        case NSStreamEvent.allZeros:
+            NSLog("allZeros")
+            break
+        case NSStreamEvent.OpenCompleted:
+            NSLog("OpenCompleted")
+            break
+        case NSStreamEvent.HasSpaceAvailable:
+            NSLog("HasSpaceAvailable")
+            break
+        default:
+            NSLog("unknown.")
+        }
+        
+    }
+    /*updateChatView()
+    
+    @input: newMessage -> message to be added on the chat view
+    @output: messages -> updates the message text view
+    */
+    func updateChatView(message:String){
         
         let date = NSDate()
         let formatter = NSDateFormatter()
         formatter.timeStyle = .MediumStyle
         formatter.stringFromDate(date)
+        messages.text! = formatter.stringFromDate(date) + "\t" + message + "\n" + messages.text!
         
-        messages.text! = formatter.stringFromDate(date) + "\t" + monTexte.text + "\n" + messages.text!;
-        
-        effacerMonTexte();
     }
-    
-    func refreshChat(){
-    
-    
-    }
+
     
     
     
-    func effacerMonTexte(){
-        monTexte.text = "";
-    }
     
-        
+    //---------Segue manengement(Transitions)-------------
+    
+    /*
+        IBAction from
+    */
     @IBAction func backToLoginView(sender: UIButton) {
-        
+       
+        inputStream.close()
+        outputStream.close()
         showFirstViewController()
-        
     }
     
-    
+    /*
+    * show the login screen
+    */
     func showFirstViewController() {
         self.performSegueWithIdentifier("idFirstSegueUnwind", sender: self)
     }
     
-    
-    
-}
+    }
 
--- Prototype_Chat/loginScreenViewController.swift
+++ Prototype_Chat/loginScreenViewController.swift
@@ -14,6 +14,8 @@ import Darwin.C
 class loginScreenViewController: UIViewController {
 
     @IBOutlet weak var lblMessage: UILabel!
+    var client:TCPClient!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,54 +37,54 @@ class loginScreenViewController: UIViewController {
     @IBOutlet weak var port: UITextField!
     @IBOutlet weak var userName: UITextField!
   
+    @IBOutlet weak var usernameReminder1: UILabel!
     
     @IBAction func connectToChat(sender: UIButton) {
-        //TODO: check for invalid entrys
-        connectToTCPServer(self.host.text, port:self.port.text.toInt()!,userName: self.userName.text)
-        showChatView()
         
-    }
-    
-    
-    func connectToTCPServer(host: String, port : Int, userName: String) -> Void{
-        //TPC socket
-       // var client:TCPClient = TCPClient(addr: host,port: port)
-        
-        var client:TCPClient = TCPClient(addr: "127.0.0.1", port: 5045)
-        //try to connect
-        var (success, errorMsg) = client.connect(timeout:1)
-        if success{
-            //premier acces au serveur avec la comande "iam"
-            var (success, errorMsg) = client.send(str:"iam:\(userName)")
-            if success{
-                var data = client.read(1024*10)
-                if let d=data{
-                    if let str=String(bytes: d, encoding: NSUTF8StringEncoding){
-                        println(str)
-                    }
-                }
-                else{
-                    println(errorMsg)
-                }
-            }
-            else {
-                println(errorMsg)
-            }
+        // checking for invalid entrys
+        if (host.text == nil || host.text.isEqual("")){
+            println("-I need the ip address to connect to the host!-")
+        }
+        else if(port.text == nil || port.text.toInt() == nil){
+            println("-If you realy want me to connect, give me the port!-")
+        }
+        else if(userName.text == nil || userName.text == ""){
+            println("-You forgot your username?-")
+            usernameReminder1.hidden = false
             
+        }else if(true){//TODO: check if the connection is possible
+            //connect
+            //disconect
+            usernameReminder1.hidden = true
+            showChatView()
         }
-        else {
-            println(errorMsg)
-        }
-    
     }
     
     
+
+    /*
+    * prepareForSegue()
+    * this function are responsable for the data transfer 
+    * between this view and th hat view!
+    */
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "idLoginSegue") {
+            var chatViewController : ViewController = segue.destinationViewController as! ViewController
+            
+            chatViewController.host = self.host.text
+            chatViewController.port = self.port.text.toInt()!
+            chatViewController.userName = self.userName.text
+        }
+    }
     
+
     
     //go to the chat view
     func showChatView(){
         self.performSegueWithIdentifier("idLoginSegue", sender: self)
     }
+    
+  
     
     //when returns from an other view
     @IBAction func returnFromSegueActions(sender: UIStoryboardSegue){
